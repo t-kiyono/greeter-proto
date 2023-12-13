8fb43ffb480160a3fba8db32a19f9ec2aa01125c
t-kiyono <t-kiyono@be-geek.net>
2023-12-13 14:06:25 +0900

    first commit

---
+++ src/main/java/net/devfit/grpcsample/greeterproto/GreeterProtoApplication.java
@@ -0,0 +1,13 @@
+package net.devfit.grpcsample.greeterproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeterProtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/devfit/grpcsample/greeterproto/GreeterProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.devfit.grpcsample.greeterproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
